132c413963e56e998fc0b5f9785f31c54a7e571e
Maxai <maxali946@gmail.com>
2018-10-26 18:00:19 +0400

    Add files via upload

--- 15__Formy__Kuki__Kanvas_1540274391/script.js
+++ 15__Formy__Kuki__Kanvas_1540274391/script.js
@@ -3,4 +3,12 @@ function submitSugnUp()
     event.preventDefault();
     let persForm = document.getElementById('userForm');
 
+    let mail = persForm.mail.value;
+    let password = persForm.password.value;
+    let repeat = persForm.repeat.value;
+
+    if (new RegExp().test(mail) == false)
+    {
+        persForm.mail.nextElementSibling.innerText = '';
+    }
 }
