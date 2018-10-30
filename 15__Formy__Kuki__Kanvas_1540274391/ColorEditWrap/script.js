033d8a64c01fd51364d0620a26d9b5ca176fea8d
Maxai <maxali946@gmail.com>
2018-10-30 17:57:59 +0400

    Add files via upload

--- 15__Formy__Kuki__Kanvas_1540274391/ColorEditWrap/script.js
+++ 15__Formy__Kuki__Kanvas_1540274391/ColorEditWrap/script.js
@@ -1,47 +1,27 @@
-import 'colorArray.js';
+function colorName(name, rgbVal, hexVal)
+{
+    this.name = name;
+    this.rgbVal = rgbVal;
+    this.hexVal = hexVal;
+}
+
+var aArr = document.getElementsByTagName('a');
+let colArr = [];
+
+for (let i = 0; i < aArr.length; ++i)
+{
+    let colName = aArr[i].text.toLowerCase();
+    ++i;
+    let colHEX = '#' + aArr[i].text;
+    ++i;
+    let colRGB = aArr[i].text;
+    
+    colArr.push(new colorName(colName, colRGB, colHEX));
+}
 
 function addColor()
 {
     event.preventDefault();
-    let colorEditCont = document.getElementById('colorEditor');
-    
-    let colorVal = colorEditCont.color.value;
-    let typeVal = colorEditCont.type.value;
-    let codeVal = colorEditCont.code.value;
-    
-    if (new RegExp(/^[a-z]{3,}$/).test(colorVal) == false)
-    {
-        colorEditCont.color.nextElementSibling.innerText = 'only letters in low case';
-    }
-    else
-    colorEditCont.color.nextElementSibling.innerText = '';
-    
-    switch (typeVal)
-    {
-        case 'RGB':
-        if (new RegExp(/^[0-255]{1,3}, [0-255]{1,3}, [0-255]{1,3}$/).test(codeVal) == false)
-        {
-            colorEditCont.code.nextElementSibling.innerText = 'RGB code must mutch the pattern [0-255], [0-255], [0-255]';
-        }
-        else
-        colorEditCont.code.nextElementSibling.innerText = '';
-        break;
-        case 'RGBA':
-        if (new RegExp(/^[0-255]{1,3}, [0-255]{1,3}, [0-255]{1,3}, (1|0?\.\d+)$/).test(codeVal) == false)
-        {
-            colorEditCont.code.nextElementSibling.innerText = 'RGB code must mutch the pattern [0-255], [0-255], [0-255], [0-1]';
-        }
-        else
-        colorEditCont.code.nextElementSibling.innerText = '';
-        break;
-        case 'HEX':
-        if (new RegExp(/^#[0-9A-Z]{6}$/).test(codeVal) == false)
-        {
-            colorEditCont.code.nextElementSibling.innerText = 'RGB code must mutch the pattern #FFFFFF';
-        }
-        else
-        colorEditCont.code.nextElementSibling.innerText = '';
-    }
 }
 
 function colorNameChange()
@@ -53,10 +33,70 @@ function colorNameChange()
     {
         colorEditCont.color.nextElementSibling.innerText = 'only letters in low case, min 3 symbol';
         document.getElementById('testColView').style.backgroundColor = 'antiquewhite';
+        colorEditCont.code.value = '';
     }
     else
     {
         colorEditCont.color.nextElementSibling.innerText = '';
+        
+        switch (colorEditCont.type.value)
+        {
+            case 'RGB':
+                colorEditCont.code.value = colArr.find(col => col.name == colorVal).rgbVal;
+            break;
+            case 'RGBA':
+                colorEditCont.code.value = colArr.find(col => col.name == colorVal).rgbVal + ',1';
+            break;
+            case 'HEX':
+                if (new RegExp(/^#[0-9A-Z]{6}$/).test(codeVal) == false)
+                {
+                    colorEditCont.code.nextElementSibling.innerText = 'RGB code must mutch the pattern #FFFFFF';
+                }
+                else
+                {
+                    colorEditCont.code.nextElementSibling.innerText = '';
+                    colorEditCont.code.value = colArr.find(col => col.name == colorVal).hexVal;
+                }
+        }
+
         document.getElementById('testColView').style.backgroundColor = colorVal;
     }
 }
+
+/*
+        switch (colorEditCont.type.value)
+        {
+            case 'RGB':
+                if (new RegExp(/^[0-255]{1,3}, [0-255]{1,3}, [0-255]{1,3}$/).test(codeVal) == false)
+                {
+                    colorEditCont.code.nextElementSibling.innerText = 'RGB code must mutch the pattern [0-255], [0-255], [0-255]';
+                }
+                else
+                {
+                    colorEditCont.code.nextElementSibling.innerText = '';
+                    colorEditCont.code.value = colArr.find(col => col.name == colorVal).rgbVal;
+                }
+            break;
+            case 'RGBA':
+                if (new RegExp(/^[0-255]{1,3}, [0-255]{1,3}, [0-255]{1,3}, (1|0?\.\d+)$/).test(codeVal) == false)
+                {
+                    colorEditCont.code.nextElementSibling.innerText = 'RGB code must mutch the pattern [0-255], [0-255], [0-255], [0-1]';
+                }
+                else
+                {
+                    colorEditCont.code.nextElementSibling.innerText = '';
+                    colorEditCont.code.value = colArr.find(col => col.name == colorVal).rgbVal;
+                }
+            break;
+            case 'HEX':
+                if (new RegExp(/^#[0-9A-Z]{6}$/).test(codeVal) == false)
+                {
+                    colorEditCont.code.nextElementSibling.innerText = 'RGB code must mutch the pattern #FFFFFF';
+                }
+                else
+                {
+                    colorEditCont.code.nextElementSibling.innerText = '';
+                    colorEditCont.code.value = colArr.find(col => col.name == colorVal).hexVal;
+                }
+        }
+ */
