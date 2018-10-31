94a3848beb3a7a32da75b4a959bf3496734c3c7a
Maxai <maxali946@gmail.com>
2018-10-31 11:53:42 +0400

    Add files via upload

--- 15__Formy__Kuki__Kanvas_1540274391/ColorEditWrap/script.js
+++ 15__Formy__Kuki__Kanvas_1540274391/ColorEditWrap/script.js
@@ -4,7 +4,7 @@ function colorName(name, rgbVal, hexVal)
     this.rgbVal = rgbVal;
     this.hexVal = hexVal;
 }
-
+//jonasjacek.github.io/colors/data.json
 var aArr = document.getElementsByTagName('a');
 let colArr = [];
 
@@ -17,11 +17,6 @@ for (let i = 0; i < aArr.length; ++i)
     let colRGB = aArr[i].text;
     
     colArr.push(new colorName(colName, colRGB, colHEX));
-}
-
-function addColor()
-{
-    event.preventDefault();
 }
 
 function colorNameChange()
@@ -42,61 +37,88 @@ function colorNameChange()
         switch (colorEditCont.type.value)
         {
             case 'RGB':
-                colorEditCont.code.value = colArr.find(col => col.name == colorVal).rgbVal;
+            colorEditCont.code.value = colArr.find(col => col.name == colorVal).rgbVal;
             break;
             case 'RGBA':
-                colorEditCont.code.value = colArr.find(col => col.name == colorVal).rgbVal + ',1';
+            colorEditCont.code.value = colArr.find(col => col.name == colorVal).rgbVal + ',1';
             break;
             case 'HEX':
-                if (new RegExp(/^#[0-9A-Z]{6}$/).test(codeVal) == false)
-                {
-                    colorEditCont.code.nextElementSibling.innerText = 'RGB code must mutch the pattern #FFFFFF';
-                }
-                else
-                {
-                    colorEditCont.code.nextElementSibling.innerText = '';
-                    colorEditCont.code.value = colArr.find(col => col.name == colorVal).hexVal;
-                }
+            colorEditCont.code.value = colArr.find(col => col.name == colorVal).hexVal;
         }
-
+        
         document.getElementById('testColView').style.backgroundColor = colorVal;
     }
 }
 
-/*
-        switch (colorEditCont.type.value)
-        {
-            case 'RGB':
-                if (new RegExp(/^[0-255]{1,3}, [0-255]{1,3}, [0-255]{1,3}$/).test(codeVal) == false)
-                {
-                    colorEditCont.code.nextElementSibling.innerText = 'RGB code must mutch the pattern [0-255], [0-255], [0-255]';
-                }
-                else
-                {
-                    colorEditCont.code.nextElementSibling.innerText = '';
-                    colorEditCont.code.value = colArr.find(col => col.name == colorVal).rgbVal;
-                }
-            break;
-            case 'RGBA':
-                if (new RegExp(/^[0-255]{1,3}, [0-255]{1,3}, [0-255]{1,3}, (1|0?\.\d+)$/).test(codeVal) == false)
-                {
-                    colorEditCont.code.nextElementSibling.innerText = 'RGB code must mutch the pattern [0-255], [0-255], [0-255], [0-1]';
-                }
-                else
-                {
-                    colorEditCont.code.nextElementSibling.innerText = '';
-                    colorEditCont.code.value = colArr.find(col => col.name == colorVal).rgbVal;
-                }
-            break;
-            case 'HEX':
-                if (new RegExp(/^#[0-9A-Z]{6}$/).test(codeVal) == false)
-                {
-                    colorEditCont.code.nextElementSibling.innerText = 'RGB code must mutch the pattern #FFFFFF';
-                }
-                else
-                {
-                    colorEditCont.code.nextElementSibling.innerText = '';
-                    colorEditCont.code.value = colArr.find(col => col.name == colorVal).hexVal;
-                }
-        }
- */
+function codeTypeChang()
+{
+    let colorEditCont = document.getElementById('colorEditor');
+    let colorVal = colorEditCont['color'].value;
+    
+    switch (colorEditCont.type.value)
+    {
+        case 'RGB':
+        colorEditCont.code.value = colArr.find(col => col.name == colorVal).rgbVal;
+        break;
+        case 'RGBA':
+        colorEditCont.code.value = colArr.find(col => col.name == colorVal).rgbVal + ',1';
+        break;
+        case 'HEX':
+        colorEditCont.code.value = colArr.find(col => col.name == colorVal).hexVal;
+    }
+}
+
+function colorCodeChange()
+{
+    // let colorEditCont = document.getElementById('colorEditor');
+    // let codeVal = colorEditCont['code'].value;
+    // let colorVal = colorEditCont['color'].value;
+
+    // switch (colorEditCont.type.value)
+    // {
+    //     case 'RGB':
+    //         if (new RegExp(/^[0-255]{1,3}, [0-255]{1,3}, [0-255]{1,3}$/).test(codeVal) == false)
+    //         {
+    //             codeVal.nextElementSibling.innerText = 'RGB code must mutch the pattern [0-255], [0-255], [0-255]';
+    //         }
+    //         else
+    //         {
+    //             codeVal.nextElementSibling.innerText = '';
+    //             colorVal.value = colArr.find(col => col.rgbVal == codeVal).name;
+    //             document.getElementById('testColView').style.backgroundColor = colorVal;
+    //         }
+    //         break;
+    //     case 'RGBA':
+    //         if (new RegExp(/^[0-255]{1,3}, [0-255]{1,3}, [0-255]{1,3}, (1|0?\.\d+)$/).test(codeVal) == false)
+    //         {
+    //             codeVal.nextElementSibling.innerText = 'RGBA code must mutch the pattern [0-255], [0-255], [0-255], [0-1]';
+    //         }
+    //         else
+    //         {
+    //             codeVal.nextElementSibling.innerText = '';
+    //             colorVal.value = colArr.find(col => col.rgbVal == codeVal).name;
+    //             document.getElementById('testColView').style.backgroundColor = colorVal;
+    //         }
+    //         break;
+    //     case 'HEX':
+    //     if (new RegExp(/^#[0-9A-Z]{6}$/).test(codeVal) == false)
+    //     {
+    //         codeVal.nextElementSibling.innerText = 'HEX code must mutch the pattern #FFFFFF';
+    //     }
+    //     else
+    //     {
+    //         codeVal.nextElementSibling.innerText = '';
+    //         colorVal.value = colArr.find(col => col.hexVal == codeVal).name;
+    //         document.getElementById('testColView').style.backgroundColor = colorVal;
+    //     }
+    // }
+}
+
+
+
+function addColor()
+{
+     
+
+    event.preventDefault();
+}
