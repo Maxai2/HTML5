48d78c0d2017501f568b2f2b841083eea337e0b8
Maxai <maxali946@gmail.com>
2018-11-14 02:50:19 +0400

    Add files via upload

--- accuWeather/Site/script.js
+++ accuWeather/Site/script.js
@@ -17,12 +17,14 @@ let findWeather = () =>
 {
     $.ajax
     ({
-        url: `http://api.openweathermap.org/data/2.5/weather?q=${$('#cityName').val()}&appid=f284eac603d628bfb6d7570e53c1567c`,
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${$('#cityName').val()}&units=metric&appid=f284eac603d628bfb6d7570e53c1567c`,
 
         success: (result, status, xhr) =>
         {
             weatherByCity = result;
             currentWeather(weatherByCity);
+            nearbyPlaces(weatherByCity.coord.lat, weatherByCity.coord.lon, weatherByCity.name);
+            $('#nowWeatherId').show();
             // console.log(status);
             // console.log(xhr);
         },
@@ -36,36 +38,151 @@ let findWeather = () =>
     })
 }
 
-let picWeather = [];
+let picWeather = {
+    '01d': '01.svg',
+    '01n': '33.svg',
+    '02d': '03.svg',
+    '02n': '35.svg',
+    '03d': '07.svg',
+    '03n': '07.svg',
+    '04d': '08.svg',
+    '04n': '08.svg',
+    '09d': '18.svg',
+    '09n': '18.svg',
+    '10d': '13.svg',
+    '10n': '40.svg',
+    '11d': '15.svg',
+    '11n': '15.svg',
+    '13d': '22.svg',
+    '13n': '22.svg',
+    '50d': '11.svg',
+    '50n': '11.svg'
+};
 
-picWeather.push(
-    {key: '01d', value: '01.svg'},
-    {key: '01n', value: '33.svg'},
-    {key: '02d', value: '03.svg'},
-    {key: '02n', value: '35.svg'},
-    {key: '03d', value: '07.svg'},
-    {key: '03n', value: '07.svg'},
-    {key: '04d', value: '08.svg'},
-    {key: '04n', value: '08.svg'},
-    {key: '09d', value: '18.svg'},
-    {key: '09n', value: '18.svg'},
-    {key: '10d', value: '13.svg'},
-    {key: '10n', value: '40.svg'},
-    {key: '11d', value: '15.svg'},
-    {key: '11n', value: '15.svg'},
-    {key: '13d', value: '22.svg'},
-    {key: '13n', value: '22.svg'},
-    {key: '50d', value: '11.svg'},
-    {key: '50n', value: '11.svg'}
-);
+function nearbyPlaces(lat, lon, city)
+{
+    $.ajax
+    ({
+        url: `http://api.openweathermap.org/data/2.5/find?lat=${lat}&lon=${lon}&cnt=10&units=metric&appid=f284eac603d628bfb6d7570e53c1567c`,
+
+        success: (result, status, xhr) =>
+        {
+            let h3 = $('<h3></h3>').text(`${city} Weather Conditions - Nearby Places`);
+            $('#nearbyPlacesId').append(h3);
+
+            let div = $('<div></div>').addClass('nearbyWeatherCl');
+
+            result.list.forEach(weath =>
+            {
+                let weathDiv = $('<div></div>').addClass('cityCl');
+
+                let label = $('<label></label>').addClass('cityNameCl').text(weath.name);
+                weathDiv.append(label);
+
+                let img = $('<img>').addClass('degreeImgCl').attr('src', `https://vortex.accuweather.com/adc2010/images/slate/icons/${picWeather[weath.weather[0].icon]}`);
+                weathDiv.append(img);
+
+                let span = $('<span></span>').addClass('degreeCl').html(`${weath.main.temp}&#186;`);
+                weathDiv.append(span);
+
+                div.append(weathDiv);
+            })
+
+            $('#nearbyPlacesId').append(div);
+        }
+    });
+}
+
 
 function currentWeather(res)
 {
-    console.log(res.weather[0].icon);
+    $('#nowId').text(`Now ${secToTime(res.dt)}`);
+    $('#nowId').css('border-bottom', '2px solid #f05514');
 
+    let div = $('<div></div>').addClass('mainContentCl');
+
+    let divLeft = $('<div></div>').addClass('leftSide');
+
+    let labelCW = $('<label></label>').addClass('mainHeader').text('Current Weather');
+    divLeft.append(labelCW);
+
+    let imgL = $('<img>').attr('src', `https://vortex.accuweather.com/adc2010/images/slate/icons/${picWeather[res.weather[0].icon]}`);
+    divLeft.append(imgL);
+
+    let labelDeg = $('<label></label>').addClass('degreeCl').html(`${res.main.temp}&#186;`);
+    let spanLab = $('<span></span>').addClass('celciusCl').text('C');
+    labelDeg.append(spanLab);
+    divLeft.append(labelDeg);
+
+    let span = $('<span></span>').addClass('realFeelCl').html(`RealFeel&reg; ${res.main.temp - 5}&#186;`);
+    divLeft.append(span);
+
+    let labelState = $('<label></label>').addClass('weatherState').text(res.weather[0].main);
+    divLeft.append(labelState);
+
+    div.append(divLeft);
+
+    let divRight = $('<div></div>').addClass('rightSide');
     
+    let vertLine = $('<div></div>').addClass('verticalLine');
+    divRight.append(vertLine);
+    
+    let sunSet = $('<div></div>').addClass('sunsetSunriseCl');
 
-    $()
+    let labelRight = $('<label></label>').addClass('mainHeader').text('Sunrise/sunset');
+    sunSet.append(labelRight);
+
+    sunSet.append($('<br><br>'));
+    
+    let spanRise = $('<span></span>').addClass('captionCl').text('Sunrise:');
+    spanRise.append($('<span></span>').addClass('valueCl').text(secToTime(res.sys.sunrise)));
+    sunSet.append(spanRise);
+
+    sunSet.append($('<br>'));
+
+    let spanSet = $('<span></span>').addClass('captionCl').text('Sunset:');
+    spanSet.append($('<span></span>').addClass('valueCl').text(secToTime(res.sys.sunset)));
+    sunSet.append(spanSet);
+    
+    sunSet.append($('<br><br>'));
+
+    let duration = secToTime(res.sys.sunset - res.sys.sunrise).replace(/\sam|pm/, '');
+
+    let spanDur = $('<span></span>').addClass('captionCl').text('Duration:');
+    spanDur.append($('<span></span>').addClass('valueCl').text(duration + ' hr'));
+    sunSet.append(spanDur);
+
+    divRight.append(sunSet);
+    
+    let imgR = $('<img>').attr('src', 'https://vortex.accuweather.com/adc2010/images/slate/icons/01.svg');
+    divRight.append(imgR);
+
+    div.append(divRight);
+    
+    $('#currentWeatherId').append(div);
+}
+
+function secToTime(sec)
+{
+    sec -= (Number.parseInt(sec / 86400) * 86400);
+    let hour = Number.parseInt(sec / 3600);
+    sec -= (hour * 3600);
+    let min = Number.parseInt(sec / 60);
+
+    let index;
+    
+    if (hour <= 12)
+    index = ' am';
+    else
+    {    
+        index = ' pm';
+        hour -= 12;
+    }
+    
+    hour += 4; // GT+4
+    let timeStr = hour + ':' + (min < 10 ? ('0' + min) : min) + index;
+
+    return timeStr;
 }
 
 function enterKey()
